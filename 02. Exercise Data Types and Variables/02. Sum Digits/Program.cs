395cbf38ca6c8d009d793fc1fa497e3ab69a93bc
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2023-09-13 15:09:40 +0300

    Add Data Types and Variables tasks for Exercise

---
+++ 02. Exercise Data Types and Variables/02. Sum Digits/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _02._Sum_Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            do
+            {
+                int finalDigits = number;
+                finalDigits %= 10;
+                number /= 10;
+                sum += finalDigits;
+            }
+
+            while (0 != number);
+                {
+                   
+                }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ 02. Exercise Data Types and Variables/10. Poke Mon/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _10._Poke_Mon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int power = int.Parse(Console.ReadLine()); //N
+            int distance = int.Parse(Console.ReadLine()); //M
+            int exhaustionFactor = int.Parse(Console.ReadLine()); //Y
+            int countOfTarget = 0;
+            double fiftyPercent = power * 0.5;
+
+            while (power >= distance)
+            {
+                power -= distance;
+                countOfTarget++;
+
+                if (fiftyPercent == power && exhaustionFactor != 0)
+                {
+                    power /= exhaustionFactor;
+                }
+            }
+            Console.WriteLine(power);
+            Console.WriteLine(countOfTarget);
+        }
+    }
+}
