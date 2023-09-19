44eb86c1d876818c879646693ff4151281cb82ad
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2023-09-19 19:06:33 +0300

    Add Objects and Classes tasks for Exercise
    Problem Solutions for Exercise: Fundamentals

---
+++ 06 Exercise Objects and Classes/06. Vehicle Catalogue/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Vehicle_Catalogue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Vehicle> vehicles = new List<Vehicle>();
+            List<Vehicle> cars = new List<Vehicle>();
+            List<Vehicle> truck = new List<Vehicle>();
+
+            GenerationVehicles(vehicles, cars, truck);
+            PrintModelData(vehicles);
+            PrintCarAverageHorsepower(vehicles, cars);
+            PrintTruckAverageHorsepower(vehicles, truck);
+        }
+        static void GenerationVehicles(List<Vehicle> vehicles, List<Vehicle> cars, List<Vehicle> truck)
+        {
+            string text;
+
+            while ((text = Console.ReadLine()) != "End")
+            {
+                string[] vehicleData = text
+                    .Split();
+
+                string type = vehicleData[0];
+                string model = vehicleData[1];
+                string color = vehicleData[2];
+                double horsepower = int.Parse(vehicleData[3]);
+
+                Vehicle vehicle = new Vehicle(type, model, color, horsepower);
+
+                if (vehicle.Type == "car")
+                {
+                    cars.Add(vehicle);
+                }
+
+                else
+                {
+                    truck.Add(vehicle);
+                }
+
+                vehicles.Add(vehicle);
+            }
+        }
+        static void PrintModelData(List<Vehicle> vehicles)
+        {
+            string text;
+
+            while ((text = Console.ReadLine()) != "Close the Catalogue")
+            {
+                string model = text;
+
+                for (int i = 0; i < vehicles.Count; i++)
+                {
+                    Vehicle currentVehicle = vehicles[i];
+
+                    if (model == currentVehicle.Model || model == currentVehicle.Color || model == currentVehicle.Horsepower.ToString())
+                    {
+                        if (currentVehicle.Type == "car")
+                        {
+                            Console.WriteLine("Type: Car");
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Type: Truck");
+                        }
+
+                        Console.WriteLine($"Model: {currentVehicle.Model}");
+                        Console.WriteLine($"Color: {currentVehicle.Color}");
+                        Console.WriteLine($"Horsepower: {currentVehicle.Horsepower}");
+                        break;
+                    }
+                }
+            }
+        }
+        static void PrintCarAverageHorsepower(List<Vehicle> vehicles, List<Vehicle> cars)
+        {
+            int carsCount = cars.Count;
+            double carsSumOfHorsepower = 0;
+            //Тук създаваме стойността на средните конски сили
+            double averageCarsSumOfHorsepower = 0;
+            for (int i = 0; i < cars.Count; i++)
+            {
+                carsSumOfHorsepower += cars[i].Horsepower;
+            }
+            //Тук добавяме една иф проверка, с която предпазваме от делене на 0
+            if (carsCount > 0)
+            {
+                averageCarsSumOfHorsepower = carsSumOfHorsepower / carsCount;
+            }
+            Console.WriteLine($"Cars have average horsepower of: {averageCarsSumOfHorsepower:F2}.");
+        }
+
+        static void PrintTruckAverageHorsepower(List<Vehicle> vehicles, List<Vehicle> truck)
+        {
+            int truckCount = truck.Count;
+            double truckSumOfHorsepower = 0;
+            //Тук създаваме стойността на средните конски сили
+            double averageTruckSumOfHorsepower = 0;
+            for (int i = 0; i < truck.Count; i++)
+            {
+                truckSumOfHorsepower += truck[i].Horsepower;
+            }
+            //Тук добавяме една иф проверка, с която предпазваме от делене на 0
+            if (truckCount > 0)
+            {
+                averageTruckSumOfHorsepower = truckSumOfHorsepower / truckCount;
+            }
+            Console.WriteLine($"Trucks have average horsepower of: {averageTruckSumOfHorsepower:F2}.");
+        }
+
+    }
+    public class Vehicle
+    {
+        public Vehicle(string type, string model, string color, double horsepower)
+        {
+            Type = type;
+            Model = model;
+            Color = color;
+            Horsepower = horsepower;
+        }
+
+        public string Type { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public double Horsepower { get; set; }
+    }
+}
---
+++ 06 Exercise Objects and Classes/07. Order by Age/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Order_by_Age
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+
+            AddPeopleInList(people);
+
+            PrintPeople(people);
+        }
+
+        static void AddPeopleInList(List<Person> people)
+        {
+            string input;
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] personInfo = input
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string currName = personInfo[0];
+                string currID = personInfo[1];
+                int currAge = int.Parse(personInfo[2]);
+
+                Person currPerson = new Person(currName, currID, currAge);
+
+
+                Person personUpdate = people.Find(x => x.ID == currID);
+
+                if (people.Contains(personUpdate))
+                {
+                    personUpdate.Name = currPerson.Name;
+                    personUpdate.Age = currPerson.Age;
+                }
+                else
+                {
+                    people.Add(currPerson);
+                }
+            }
+        }
+
+        static void PrintPeople(List<Person> people)
+        {
+            people = people.OrderBy(x => x.Age).ToList();
+
+            foreach (Person person in people)
+            {
+                Console.WriteLine(person.ToString());
+            }
+        }
+    }
+
+    public class Person
+    {
+        public Person(string name, string id, int age)
+        {
+            Name = name;
+            ID = id;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+
+        public string ID { get; set; }
+
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} with ID: {ID} is {Age} years old.";
+        }
+    }
+}
