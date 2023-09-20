eb32b6e7da5b672236be7d3a1dc59e8a7f563222
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2023-09-20 21:37:17 +0300

    Add Associative Arrays tasks for Exercise
    Problem Solutions for Exercise: Fundamentals

---
+++ 07 Exercise Associative Arrays/06. Student Academy/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Student_Academy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberPair = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<double>> listGrades =
+                new Dictionary<string, List<double>>();
+
+            string name = string.Empty;
+            double grade = 0.00;
+
+            for (int i = 0; i < numberPair; i++)
+            {
+                name = Console.ReadLine();
+                grade = double.Parse(Console.ReadLine());
+
+                if (!listGrades.ContainsKey(name))
+                {
+                    listGrades[name] = new List<double>();    
+                }
+
+                listGrades[name].Add(grade);
+            }
+
+            double value = 4.50;
+
+            foreach (var item in listGrades)
+            {
+                if (item.Value.Average() >= value)
+                {
+                    Console.WriteLine($"{item.Key} -> {item.Value.Average():f2}");
+                }
+            }
+
+        }
+    }
+}
---
+++ 07 Exercise Associative Arrays/07. Company Users/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07._Company_Users
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> companyInfo =
+                new Dictionary<string, List<string>>();
+
+            string company;
+            while ((company = Console.ReadLine()) != "End")
+            {
+                string[] cmdArgs = company
+                     .Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+
+                string companyName = cmdArgs[0];
+                string id = cmdArgs[1];
+
+                if (!companyInfo.ContainsKey(companyName))
+                {
+                    companyInfo.Add(companyName, new List<string>());
+                }
+                if (!companyInfo[companyName].Contains(id))
+                {
+                    companyInfo[companyName].Add(id);
+                }
+            }
+
+            foreach (var kvp in companyInfo)
+            {
+                string companyName = kvp.Key;
+                List<string> idN = kvp.Value;
+
+                Console.WriteLine($"{companyName}");
+                foreach (string id in idN)
+                {
+                    Console.WriteLine($"-- {id}");
+                }
+            }
+        }
+    }
+}
