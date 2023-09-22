937126097ebcaa2602ccb67007d0e17d663dec03
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2023-09-22 23:16:42 +0300

    Mid Exam

---
+++ Mid Exam 8.12.22/02. Book’s World/02. Book’s World/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Book_s_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var genre = Console.ReadLine()
+                .Split(" | ")
+                .ToList();
+
+            string input;
+            while ((input = Console.ReadLine()) != "Stop!")
+            {
+                string[] cmdArgs = input
+                     .Split(" "/*, StringSplitOptions.RemoveEmptyEntries*/);
+
+                string cmdType = cmdArgs[0];
+
+                if (cmdType == "Join")
+                {
+                    string nameGenre = cmdArgs[1];
+                    if(!genre.Contains(nameGenre))
+                    { 
+                    genre.Add(nameGenre);
+                    }
+                }
+                else if (cmdType == "Drop")
+                {
+                    string nameGenre = cmdArgs[1];
+                    if (genre.Contains(nameGenre))
+                    {
+                        genre.Remove(nameGenre);
+                    }
+
+                }
+                else if (cmdType == "Replace")
+                {
+                    string oldGenre = cmdArgs[1];
+                    string newGenre = cmdArgs[2];
+                    if (genre.Contains(oldGenre) && !genre.Contains(newGenre))
+                    {
+                        int oldNameIndex = genre.IndexOf(oldGenre);
+                        genre[oldNameIndex] = newGenre;
+                    }
+                }
+                else if (cmdType == "Prefer")
+                {
+                    int indexOne = int.Parse(cmdArgs[1]);
+                    int indexTwo = int.Parse(cmdArgs[2]);
+                    if (indexOne >= 0 && indexOne < genre.Count && indexTwo >= 0 && indexTwo < genre.Count)
+                    {
+                        if (genre.Contains(genre[indexOne]) && genre.Contains(genre[indexTwo]))
+                        {
+                            string tmp = genre[indexOne];
+                            genre[indexOne] = genre[indexTwo];
+                            genre[indexTwo] = tmp;
+                        }
+                    }
+                }
+            }
+           Console.WriteLine(string.Join(" ", genre));
+        }
+    }
+}
---
+++ Mid Exam 8.12.22/03. Magic Cards/03. Magic Cards/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+ namespace _03._Magic_Cards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var deck = Console.ReadLine()
+                 .Split(":")
+                 .ToList();
+            var newDeck = new List<string>();
+
+            string input;
+            while ((input = Console.ReadLine()) != "Ready")
+            {
+                string[] cmdArgs = input
+                     .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string cmdType = cmdArgs[0];         
+
+                if (cmdType == "Add")
+                {
+                    string cardName = cmdArgs[1];
+                    if (deck.Contains(cardName))
+                    {                       
+                       newDeck.Add(cardName);                                              
+                    }
+                    else
+                    {
+                        Console.WriteLine("Card not found.");
+                    }
+                }
+                else if (cmdType == "Insert")
+                {
+                    string cardName = cmdArgs[1];
+                    int index = int.Parse(cmdArgs[2]);
+                    if (deck.Contains(cardName) && (index >= 0 && index < newDeck.Count))
+                    {                        
+                            newDeck.Insert(index, cardName); 
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error!");
+                    }
+                }
+                else if (cmdType == "Remove")
+                {
+                    string cardName = cmdArgs[1];
+                    if (newDeck.Contains(cardName))
+                    {
+                        newDeck.Remove(cardName);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Card not found.");
+                    }
+                    continue;
+                }
+                else if (cmdType == "Swap")
+                {
+                    string cardName = cmdArgs[1];
+                    string newCardName = cmdArgs[2];
+                    if (cardName != newCardName)
+                    {
+                        int indexOne = newDeck.IndexOf(cardName);
+                        int indexTwo = newDeck.IndexOf(newCardName);
+
+                        string tmp = newDeck[indexOne];
+                        newDeck[indexOne] = newDeck[indexTwo];
+                        newDeck[indexTwo] = tmp;
+                    }
+                    continue;
+                }
+
+                else if (cmdType == "Shuffle")
+                {
+                    string cmdType2 = cmdArgs[1];
+                    if (cmdType2 == "deck")
+                    {
+                        newDeck.Reverse();
+                    }              
+                }
+            }
+           Console.WriteLine(string.Join(" ", newDeck));
+        }
+    }
+}
