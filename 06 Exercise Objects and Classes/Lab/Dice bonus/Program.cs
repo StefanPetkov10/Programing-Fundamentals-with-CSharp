eaac4d700e5ee6bd5142042b6408bb695d3fe7a5
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2023-11-11 15:48:08 +0200

    Add Object and Classes tasks - Lab
    Problem Solutions Exercise: Fundamentals

---
+++ 06 Exercise Objects and Classes/Lab/Dice bonus/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Dice_bonus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dice dice = new Dice();//6
+            dice.Sides = 6;
+            dice.Roll();
+            Console.WriteLine(dice.SideName);
+        }
+    }
+
+    public class Dice
+    {
+        //public Dice(int sides)
+        //{
+        //    Sides = sides;
+        //}
+        public string SideName
+        {
+            get
+            {
+                return $"My side name is {Sides}";
+            }
+        }
+        public int Sides { get; set; }
+        public void Roll()
+        {
+            Random random = new Random();
+            int rndNumber = random.Next(1, Sides + 1);
+            Console.WriteLine(rndNumber);
+        }
+    }
+}
---
+++ 06 Exercise Objects and Classes/Lab/Student/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Student
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("StePet", 26);
+
+            Console.WriteLine(student.Name);
+            Console.WriteLine(student.Age);
+        }
+    }
+
+    public class Student
+    {
+        public Student(string name, int age)
+        {
+            Name = name;             //Конструктора се използва за инециализация на нашия клас/ на нащия обект
+            Age = age;              //Ползваме ги да създаваме обекти
+        }                          
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+       }
+        
+    }
+
+
