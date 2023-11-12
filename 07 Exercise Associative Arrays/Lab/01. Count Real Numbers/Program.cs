50afa20b40c8df2eae3e084b68042e4c4348f56a
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2023-11-12 23:48:57 +0200

    Add Associative Arrays tasks - Lab
    Problem Solutions Exercise: Fundamentals

---
+++ 07 Exercise Associative Arrays/Lab/01. Count Real Numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace _01._Count_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<double, int> numberOcc    //var numberOcc
+                = new SortedDictionary<double, int>();
+
+            double[] inputNumber = Console.ReadLine()
+            .Split()
+            .Select(double.Parse)
+            .ToArray();
+
+            foreach (var currNum in inputNumber)
+            {
+                if (!numberOcc.ContainsKey(currNum))
+                {
+                    numberOcc.Add(currNum, 0);
+                }
+
+                numberOcc[currNum] += 1;
+            }
+
+            foreach (var (key, value) in numberOcc)
+            {
+                Console.WriteLine($"{key} -> {value}");
+            }
+        }
+    }
+}
---
+++ 07 Exercise Associative Arrays/Lab/01. Count Real Numbers/SortedByDictionary.cs
@@ -0,0 +1,6 @@
+﻿namespace _01._Count_Real_Numbers
+{
+    internal class SortedByDictionary<T1, T2>
+    {
+    }
+}
