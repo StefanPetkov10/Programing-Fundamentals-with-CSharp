c2ab6da2bd33b377ac9cc72f6a415f154d522c61
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2023-11-12 23:50:29 +0200

    Add Associative Arrays tasks - Lab
    Problem Solutions Exercise: Fundamentals

---
+++ 07 Exercise Associative Arrays/Lab/03. Word Synonyms/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _03._Word_Synonyms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var synonyms = new Dictionary<string, List<string>>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+
+                if (!synonyms.ContainsKey(word))
+                {
+                    synonyms.Add(word, new List<string>());
+                }
+
+                synonyms[word].Add(synonym);
+            }
+
+            foreach (var item in synonyms)
+            {
+                Console.WriteLine($"{item.Key} - {string.Join(", ", item.Value)}");
+            }
+
+        }
+    }
+}
+
+
+//static void Main(string[] args)
+//{
+//    Dictionary<string, Dictionary<string, string>> synonums
+//         = new Dictionary<string, Dictionary<string, string>>();
+
+//    var dictForWord = new Dictionary<string, string>();
+//    synonums.Add("cute", dictForWord //or new Dictionary<string, string>());
+//    synonums["cute"].Add("sth", "else");
+
+//    foreach (var item in synonums)
+//    {
+//        Console.WriteLine(item.Key);
+
+//        foreach (var val in item.Value)
+//        {
+//            Console.WriteLine($"{val.Key} - {val.Value}");
+//        }
+//    }
+//}
---
+++ 07 Exercise Associative Arrays/Lab/04. Word Filter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _04._Word_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine()
+                .Split()
+                .Where(x => x.Length % 2 == 0)
+                .ToList()
+                .ForEach(Console.WriteLine);
+        }
+    }
+}
