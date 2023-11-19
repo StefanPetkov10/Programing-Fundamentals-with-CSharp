fbf1007dd772335f6f23eaf7625b401847593500
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2023-11-19 12:45:41 +0200

    Add Text Processing tasks - Lab
    Problem Solutions Exercise: Fundamentals

---
+++ 08 Exercise Text Processing/Lab/02. Repeat Strings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Repeat_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split();
+            string result = string.Empty; 
+
+            foreach (var currentWord in words)
+            {
+                for (int i = 0; i < currentWord.Length; i++)
+                {
+                    result += currentWord;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
